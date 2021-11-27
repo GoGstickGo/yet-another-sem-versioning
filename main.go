66c30f87875ee0209284a20aec0d84b922c4f9a9
GoGstickGo <janilution@gmail.com>
2021-11-27 20:29:22 +0000

    refactor: add more functions

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,32 +11,42 @@ import (
 )
 
 func main() {
-	cVersion, err := readInFile("VERSION")
+	file := os.Args[1]
+	change := os.Args[2]
+
+	err := checkCommitMsg(change)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
-	fmt.Println(cVersion)
-	change := "patch"
-	newVersion := createNewVersion(cVersion, change)
-	fmt.Println(newVersion)
-	err = modifyVersionFile(newVersion)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-}
-
-func readInFile(file string) (cVersion string, err error) {
-	currentVersion, err := ioutil.ReadFile(file)
+	err = checkVersionFileExist(file)
 	if err != nil {
-		return "", fmt.Errorf("issue with reading the file")
-	}
-	if strings.HasPrefix(string(currentVersion), "v") == true {
-		return string(currentVersion[1:]), nil
+		fmt.Printf("%v \n", err)
+		err = createVersionFile(file, change)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
 	} else {
-		return string(currentVersion[0:]), nil
+		cVersion, vform, err := readInFile(file)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+
+		newVersion := buildNewVersion(cVersion, change)
+
+		err = modifyVersionFile(newVersion, file, vform)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
 	}
 }
+
+const (
+	p1 = "major"
+	p2 = "minor"
+	p3 = "patch"
+)
+
 func modifyVersion(a, b, c int, s []string) (mVersion string) {
 	mV, _ := strconv.Atoi(s[a])
 	mV++
@@ -51,7 +62,7 @@ func modifyVersion(a, b, c int, s []string) (mVersion string) {
 	return mVersion
 }
 
-func createNewVersion(cVersion, change string) (nVersion string) {
+func buildNewVersion(cVersion, change string) (nVersion string) {
 	v := strings.Split(cVersion, ".")
 	switch change {
 	case "major":
@@ -67,17 +78,69 @@ func createNewVersion(cVersion, change string) (nVersion string) {
 	return nVersion
 }
 
-func modifyVersionFile(nVersion string) (err error) {
-	// remove current file
-	err = os.Remove("VERSION")
+func readInFile(file string) (cVersion string, vform bool, err error) {
+	currentVersion, err := ioutil.ReadFile(file)
 	if err != nil {
-		return fmt.Errorf("issue occured when deleting the old version file, error: %v", err)
+		return "", false, fmt.Errorf("issue with reading the file in")
 	}
-	// create new file & modify
-	bs := []byte("v" + nVersion)
-	err = ioutil.WriteFile("VERSION", bs, 0744)
+	if strings.HasPrefix(string(currentVersion), "v") {
+		return string(currentVersion[1:]), true, nil
+	} else {
+		return string(currentVersion[0:]), false, nil
+	}
+}
+
+func createVersionFile(filePath, change string) (err error) {
+	cVersion := "0.0.0"
+	nVersion := buildNewVersion(cVersion, change)
+	bs := []byte(nVersion)
+	err = ioutil.WriteFile(filePath, bs, 0744)
 	if err != nil {
-		return fmt.Errorf("issue occured with new version file error: %v", err)
+		return fmt.Errorf("issue occured when writing to the new version file")
 	}
 	return nil
+}
+
+func modifyVersionFile(nVersion, fileName string, vform bool) (err error) {
+	var bs []byte
+	// remove current file
+	err = deleteOldVersionFile(fileName)
+	if err != nil {
+		return err
+	}
+	// create new file & modify
+	if vform {
+		bs = []byte("v" + nVersion)
+	} else {
+		bs = []byte(nVersion)
+	}
+	err = ioutil.WriteFile(fileName, bs, 0755)
+	if err != nil {
+		return fmt.Errorf("issue occured when writing to the new version file")
+	}
+	return nil
+}
+
+func deleteOldVersionFile(filePath string) (err error) {
+	err = os.Remove(filePath)
+	if err != nil {
+		return fmt.Errorf("issue occured when deleting the old version file, error")
+	}
+	return nil
+}
+
+func checkVersionFileExist(filePath string) (err error) {
+	_, err = os.Open(filePath)
+	if errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("version file doesn't exist..creating one")
+	}
+	return nil
+}
+
+func checkCommitMsg(commit string) (err error) {
+	if strings.Contains(commit, p1) || strings.Contains(commit, p2) || strings.Contains(commit, p3) {
+		return nil
+	}
+	return fmt.Errorf("please add major, minor or patch to commit msg")
+
 }
